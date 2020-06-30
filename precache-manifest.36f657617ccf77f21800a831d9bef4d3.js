725edc41c4844f1d87577f243f4333a73511e308
unknown <you@example.comrimcssiste@gmail.com git config --global user.email you@example.comrimcssiste@gmail.com>
2020-06-30 22:06:27 +0200

    Updates

---
+++ precache-manifest.36f657617ccf77f21800a831d9bef4d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b64a3e7664efe6d5f45cb9d71f70478",
+    "url": "/react_jsx/index.html"
+  },
+  {
+    "revision": "323d847ba18da4e5f594",
+    "url": "/react_jsx/static/css/main.87ca3021.chunk.css"
+  },
+  {
+    "revision": "7aff6fd0cfb4796906dd",
+    "url": "/react_jsx/static/js/2.9e35905b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_jsx/static/js/2.9e35905b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323d847ba18da4e5f594",
+    "url": "/react_jsx/static/js/main.6ad41cdc.chunk.js"
+  },
+  {
+    "revision": "0a99433d6cdfff886c68",
+    "url": "/react_jsx/static/js/runtime-main.d6acad37.js"
+  },
+  {
+    "revision": "891f8bde1f29df7afb07c0e7f342e1dc",
+    "url": "/react_jsx/static/media/imageInSrc.891f8bde.jpg"
+  },
+  {
+    "revision": "335a792d717b60066f79a9be15871e06",
+    "url": "/react_jsx/static/media/myVideo.335a792d.mp4"
+  }
+]);
